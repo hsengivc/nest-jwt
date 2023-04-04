f67f6a453098ed20d0eac4f9e18bef4c7d5635f6
hsengivc <hsengivc@proton.me>
2023-04-04 06:50:43 +0530

    add jwt passport startegies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AtStrategy } from './strategies';
+import { RtStrategy } from './strategies/rt.startegy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, AtStrategy, RtStrategy],
 })
 export class AuthModule {}
